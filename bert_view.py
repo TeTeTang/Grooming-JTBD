e043c815846ef895f06b472f7bea7dda2455936a
TeTeTang <tang.p.3@pg.com>
2023-01-06 11:39:27 -0800

    streamlit app code added

---
+++ bert_view.py
@@ -0,0 +1,24 @@
+from torch.cuda import is_available
+
+from simpletransformers.classification import ClassificationModel, ClassificationArgs
+
+model_class_map = {"ClassificationModel": "Classification Model"}
+
+# Optional model configuration
+model_args = ClassificationArgs(num_train_epochs=5)
+# Load the trained model
+loaded_model = ClassificationModel("bert", "bert_model", args=model_args)  
+
+# Inference 
+def inference(df):
+    input_text = df["text"].tolist()
+    predictions, logprobs = loaded_model.predict(input_text)
+    return predictions, logprobs
+
+
+
+# text_relevant = []
+# for idx, text in enumerate(input_list):
+#     t = (text, predictions[idx])
+#     text_relevant.append(t)
+
